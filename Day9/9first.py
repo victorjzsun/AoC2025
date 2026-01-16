e5a880874b17c86db79531cb174a7b6c607d7088
Victor Sun <victorjsun@gmail.com>
2026-01-16 01:14:30 -0500

    Day 9

--- Day9/9first.py
+++ Day9/9first.py
@@ -1,4 +1,14 @@
+def area(a, b):
+  return (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1)
+
 coords = []
 with open("input.txt") as fIn:
   for line in fIn:
-    
+    coords.append(tuple(int(x) for x in line.strip().split(",")))
+
+maxArea = -1
+for i in coords:
+  for j in coords:
+    maxArea = max(maxArea, area(i,j))
+
+print(maxArea)
